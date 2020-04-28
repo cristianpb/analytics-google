048245db4992826b38f1e184ef96b4d3cf4e74ae
cristianpb <felipebrokate@gmail.com>
2020-04-28 08:09:50 +0200

    💥 Use leaflet API instead of mapbox

--- src/components/views/Map.svelte
+++ src/components/views/Map.svelte
@@ -1,86 +1,74 @@
-<p>
-  Map
-</p>
-
-<div id="map" style="width: 100%; height: 400px"></div>
+<div id="map"  bind:this={mapContainer} style="width: 100%; height: 400px"></div>
 
 <script>
-  import { data as dataCsv } from '../tools/stores.js';
-  import mapboxgl from 'mapbox-gl';
+  import { data as dataCsv, results } from '../tools/stores.js';
+  import L from "leaflet";
   import { onMount } from "svelte";
 
-  let map
+  let leafletMap;
+  let mapContainer;
+  let countriesLayer;
+  let center = [20, 15.8];
+  let zoom = 1;
 
   onMount(async () => {
-    mapboxgl.accessToken = '__MAPBOX_TOKEN__';
-    map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9'
+    leafletMap = L.map(mapContainer, {
+      svgSprite: false,
+      preferCanvas: false,
+      zoomControl: false,
+      center: center,
+      zoom: zoom
     });
+    leafletMap.doubleClickZoom.disable();
+    L.control.zoom({
+        position: 'topright'
+    }).addTo(leafletMap);
+
     const res = await fetch(`__BASEURL__/countries.geo.json`);
     const countries = await res.json();
-    const unsubscribe = dataCsv.subscribe(myData => {
+
+    const unsubscribe = results.subscribe(myData => {
       if (myData.length > 0) {
-        console.log('CHANGE',  myData.length);
-        map.on('load', function() {
-          countries.features.forEach((obj) => {
-            let density = myData
-              .filter(x => x.countryIsoCode === obj.id)
-              .reduce((total, s) => total + s.sessions, 0)
-            obj.properties.density = density;
-          })
-          drawMap(countries)
-          map.on('click', 'path-countries', function(e) {
-            new mapboxgl.Popup()
-              .setLngLat(e.lngLat)
-              .setHTML(`${e.features[0].properties.name}: ${e.features[0].properties.density}`)
-              .addTo(map);
-          });
+        console.log('Change',  myData.length);
+        countries.features.forEach((obj) => {
+          let density = myData
+            .filter(x => x.countryIsoCode === obj.id)
+            .reduce((total, s) => total + s.sessions, 0)
+          obj.properties.density = density;
+          if (density > 0) console.log(`${obj.id} - ${density}`);
         })
+        leafletMap.eachLayer(l => leafletMap.removeLayer(l))
+        countriesLayer = L.geoJson(countries, {
+          style, 
+          onEachFeature: (f, l) => l.bindPopup(`${f.properties.name}: ${f.properties.density}`)
+          })
+        countriesLayer.addTo(leafletMap);
       }
     })
-
   });
 
-  function drawMap(countries) {
-    map.setCenter([20,15.8]);
-    map.setZoom(0);
-    map.addSource('countries', {
-      type: 'geojson',
-      data: countries
-    })
+  function getColor(d) {
+    return d > 1000 ? '#800026' :
+      d > 120  ? '#723122' :
+      d > 100  ? '#8B4225' :
+      d > 75  ? '#A25626' :
+      d > 40  ? '#B86B25' :
+      d > 20  ? '#CA8323' :
+      d > 10   ? '#DA9C20' :
+      d > 5   ? '#E6B71E' :
+      d >= 1   ? '#EED322' :
+      'white';
+  }
 
-    map.addLayer({
-      'id': 'path-countries',
-      'type': 'fill',
-      'source': 'countries',
-      'paint': {
-        'fill-color': [
-          'interpolate',
-          ['linear'],
-          ['get', 'density'],
-          0,
-          '#F2F12D',
-          10,
-          '#EED322',
-          20,
-          '#E6B71E',
-          30,
-          '#DA9C20',
-          40,
-          '#CA8323',
-          50,
-          '#B86B25',
-          75,
-          '#A25626',
-          100,
-          '#8B4225',
-          120,
-          '#723122'
-        ],
-        'fill-opacity': 0.75
-      }
-    })
+  function style(feature) {
+    return {
+      fillColor: getColor(feature.properties.density),
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    };
   }
 
 </script>
