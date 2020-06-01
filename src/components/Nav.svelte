ff2882dc7e2f8e7022178a1e36f5b680364687a0
cristianpb <felipebrokate@gmail.com>
2020-06-01 18:16:05 +0200

    🎨 Add navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,64 @@
 <script>
 	export let segment;
+
+  import SearchBox from './views/SearchBox.svelte';
+  import { loadCsv } from './tools/getData';
+  import { results } from './tools/stores.js'
+  let total
+  let burgerState = false;
+
+  const unsubscribe = results.subscribe(myData => {
+    total = myData.reduce((total, s) => total + s.sessions, 0);
+  });
+
+  async function resetData() {
+    const myData = await loadCsv()
+    $results = myData
+    console.log("RESET", $results.length);
+  }
+
+  function toggleBurger() {
+    burgerState = !burgerState
+  }
+
 </script>
 
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+<nav class="navbar is-link is-fixed-top" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <div href="{null}" class="navbar-item">
+      <a href="{null}" class="button" on:click={resetData}>
+        ♻️
+      </a>
+    </div>
+    <div class="navbar-item">
+      <SearchBox/>
+    </div>
+    <a href="{null}" 
+       class:is-active={burgerState}
+       on:click={toggleBurger}
+       role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-
-<nav>
-	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
-	</ul>
+  <div id="navMenu" class="navbar-menu" class:is-active={burgerState}>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-danger" aria-current='{segment === undefined ? "page" : undefined}' href='.'>
+            home
+          </a>
+        </div>
+      </div>
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-danger" href="https://github.com/cristianpb/analytics-google">
+            Github code
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </nav>
--- src/components/views/Layout.svelte
+++ src/components/views/Layout.svelte
@@ -1,11 +1,9 @@
 <div class="app">
-  <Header/>
   <Charts/>
   <Footer/>
 </div>
 
 <script>
-  import Header from './Header.svelte';
   import Charts from './Charts.svelte';
   import Footer from './Footer.svelte';
 </script>
