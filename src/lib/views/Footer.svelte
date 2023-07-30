280deb5e19039320ff87ba1a55dcc38f40680e61
cristianpb <felipebrokate@gmail.com>
2023-07-30 22:58:26 +0200

    🎨 Update footer to tailwind style

--- src/lib/views/Footer.svelte
+++ src/lib/views/Footer.svelte
@@ -1,12 +1,12 @@
-<footer class="footer">
-	<div class="content has-text-centered">
-		<p>
+<footer class="m-4 bg-white rounded-lg shadow dark:bg-gray-900">
+    <div class="w-full p-4 mx-auto max-w-screen-xl md:py-8">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
 			This dashboard shows the traffic data from <a href="https://cristianpb.github.io"
 				>cristianpb.github.io</a
 			>. The code source of this dashboard is published in
 			<a class="is-danger" href="https://github.com/cristianpb/analytics-google">
 				this github repository
 			</a>
-		</p>
-	</div>
+    </div>
 </footer>
